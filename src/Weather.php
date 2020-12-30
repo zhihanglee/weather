cc574ef680a8684cd83fc8c7e0dc03a6543c06e1
vagrant <vagrant@homestead>
2020-12-30 07:16:22 +0000

    Test StyleCI.

--- src/Weather.php
+++ src/Weather.php
@@ -92,9 +92,8 @@ class Weather
      * @throws InvalidArgumentException
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function getWeather($city, $type = 'base', $format = 'json')
-    {
-        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+    public function getWeather($city,$type = 'base',$format = 'json')
+    {$url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$format);
