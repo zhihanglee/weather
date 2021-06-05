0e6a1d205308906e720da31a4710458113d5c4ba
vagrant <vagrant@homestead>
2021-06-05 01:28:15 +0000

    更新版本依赖以及测试注释优化

--- src/Weather.php
+++ src/Weather.php
@@ -12,6 +12,7 @@
 namespace Leo0315\Weather;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Leo0315\Weather\Exceptions\HttpException;
 use Leo0315\Weather\Exceptions\InvalidArgumentException;
 
@@ -28,7 +29,6 @@ class Weather
 
     /**
      * Weather constructor.
-     *
      * @param string $key
      */
     public function __construct($key)
@@ -39,92 +39,81 @@ class Weather
     /**
      * @return Client
      */
-    public function getHttpClient()
+    public function getHttpClient(): Client
     {
         return new Client($this->guzzleOptions);
     }
 
-    public function setGuzzleOptions(array $options)
+    public function setGuzzleOptions(array $options): void
     {
         $this->guzzleOptions = $options;
     }
 
     /**
-     * 获取实时天气.
-     *
+     * 返回实时天气
      * @param $city
      * @param string $format
-     *
      * @return mixed|string
-     *
      * @throws HttpException
-     * @throws InvalidArgumentException|\GuzzleHttp\Exception\GuzzleException
+     * @throws InvalidArgumentException|GuzzleException
      */
-    public function getLiveWeather($city, $format = 'json')
+    public function getLiveWeather($city, string $format = 'json')
     {
         return $this->getWeather($city, 'base', $format);
     }
 
     /**
-     * 获取天气预报.
-     *
+     * 返回近几天的天气预报
      * @param $city
      * @param string $format
-     *
      * @return mixed|string
-     *
      * @throws HttpException
-     * @throws InvalidArgumentException|\GuzzleHttp\Exception\GuzzleException
+     * @throws InvalidArgumentException|GuzzleException
      */
-    public function getForecastsWeather($city, $format = 'json')
+    public function getForecastsWeather($city, string $format = 'json')
     {
         return $this->getWeather($city, 'all', $format);
     }
 
     /**
+     * 获取天气主接口
      * @param $city
      * @param string $type
      * @param string $format
-     *
      * @return mixed|string
-     *
      * @throws HttpException
      * @throws InvalidArgumentException
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws GuzzleException
      */
-    public function getWeather($city, $type = 'base', $format = 'json')
+    public function getWeather($city, string $type = 'base', string $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: '.$format);
+            throw new InvalidArgumentException('Invalid response format: ' . $format);
         }
-
         if (!\in_array(\strtolower($type), ['base', 'all'])) {
-            throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
+            throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
         }
-
         $format = \strtolower($format);
         $type = \strtolower($type);
-        // 2. 封装 query 参数，并对空值进行过滤。
+        //封装 query 参数，并对空值进行过滤。
         $query = array_filter([
-            'key' => $this->key,
-            'city' => $city,
-            'output' => $format,
+            'key'        => $this->key,
+            'city'       => $city,
+            'output'     => $format,
             'extensions' => $type,
         ]);
-
         try {
-            // 3. 调用 getHttpClient 获取实例，并调用该实例的 `get` 方法，
-            // 传递参数为两个：$url、['query' => $query]，
+            //调用 getHttpClient 获取实例，并调用该实例的 `get` 方法，
             $response = $this->getHttpClient()->get($url, [
                 'query' => $query,
             ])->getBody()->getContents();
-            // 4. 返回值根据 $format 返回不同的格式，
-            // 当 $format 为 json 时，返回数组格式，否则为 xml。
+            //返回值根据 $format 返回不同的格式，
+            //当 $format 为 json 时，返回数组格式，否则为 xml。
             return 'json' === $format ? \json_decode($response, true) : $response;
         } catch (\Exception $e) {
-            // 5. 当调用出现异常时捕获并抛出，消息为捕获到的异常消息，
-            // 并将调用异常作为 $previousException 传入。
+            //当调用出现异常时捕获并抛出，消息为捕获到的异常消息，
+            //并将调用异常作为 $previousException 传入。
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
